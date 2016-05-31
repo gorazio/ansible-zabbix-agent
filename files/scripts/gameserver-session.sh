2e1e5587438196788b80ee8f21173a75805c9e2a
Stepanov Mihail <gmail@gorazio.com>
2016-05-31 11:44:32 +0300

    fix gameserver script

--- files/scripts/gameserver-session.sh
+++ files/scripts/gameserver-session.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 ### DESCRIPTION
-# $1 - access key
-# $2 - port
-# $3 - metric
+# $1 - user
+# $2 - password
+# $3 - port
+# $4 - metric
 
 
 ### OPTIONS VERIFICATION
-[[ $# -lt 3 ]] && { echo "FATAL: some parameters not specified"; exit 1; }
+[[ $# -lt 4 ]] && { echo "FATAL: some parameters not specified"; exit 1; }
 
 ### PARAMETERS
-ACCESSKEY="$1"
-PORT="$2"
-METRIC="$3"
+USER="$1"
+PASSWORD="$2"
+PORT="$3"
+METRIC="$4"
 
 CACHETTL="10" # cache time in seconds
 CACHE="/tmp/gameserver-session"
@@ -31,6 +33,7 @@ TIMENOW=`/bin/date '+%s'`
 
 # If cache too old - update it
 if [ "$(($TIMENOW - $TIMECACHE))" -gt "$CACHETTL" ]; then
+    ACCESSKEY=`$CURL -s -X POST -u "$USER:$PASSWORD" 127.0.0.1:$PORT/api/slots.auth | jq .response.accessKey | sed -e 's/^"//'  -e 's/"$//'`
     $CURL -s --insecure "127.0.0.1:$PORT/api/slots.getSessionsCount?accessKey=$ACCESSKEY" > $CACHE || exit 1
 fi
 
